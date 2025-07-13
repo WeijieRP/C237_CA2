684365083268f5c2c89b7fb7b49ac260776cc246
weijie <hooiweijie60@gmail.com>
2025-07-14 00:08:58 +0800

    inital commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const multer = require("multer");
+const mysql = require("mysql2")
+const app = express();
+
+app.use(express.json());
+
+const sql =mysql.createConnection({
+    host: 'localhost',
+    password:"",
+    user:"root",
+    database:"testdb",
+})
+sql.connect((err) => { 
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+})
+
+
+
+
+
+app.listen(3000, () => {  
+    console.log('Server is running on port 3000');
+});
