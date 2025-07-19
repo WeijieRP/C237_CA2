e2aa527baa3996d435b7d852f99c95a4612fb9dc
WeijieRP <24040351@myrp.edu.sg>
2025-07-19 23:20:25 +0800

    Delete Nodejs directory

--- Nodejs/logger.js
+++
@@ -1,9 +0,0 @@
-const logger=(req , res, next)=>{
-    const method = req.method
-    const url = req.url
-    const date = new Date().getFullYear()
-    console.log(method , url ,date)
-    //midle 
-    next()
-}
-module.exports= logger
--- Nodejs/routing/products.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const routers= express.Router()
-const {products , getproduct}= require("../controller/products")
-
-routers.post("/create",products );
-routers.get("/items/:Id",getproduct)
-module.exports = routers
